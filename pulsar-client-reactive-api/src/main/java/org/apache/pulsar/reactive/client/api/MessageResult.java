d3fce7d7f2242c5fb3e2a11fe55a3d66f0d374d3
Christophe Bornet <cbornet@hotmail.com>
2022-11-28 17:27:44 +0100

    Add javadoc to MessageResult related classes (#48)

--- pulsar-client-reactive-api/src/main/java/org/apache/pulsar/reactive/client/api/MessageResult.java
+++ pulsar-client-reactive-api/src/main/java/org/apache/pulsar/reactive/client/api/MessageResult.java
@@ -20,40 +20,104 @@ import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageId;
 import org.apache.pulsar.reactive.client.internal.api.ApiImplementationFactory;
 
+/**
+ * Result of a message processing.
+ *
+ * @param <T> the message payload type
+ * @author Lari Hotari
+ * @author Christophe Bornet
+ */
 public interface MessageResult<T> {
 
+	/**
+	 * Returns the processed value and signals that the message must be acknowledged.
+	 * @param <T> the type of the processed value
+	 * @param messageId the id of the message to acknowledge
+	 * @param value the processed value
+	 * @return the result of the message processing
+	 */
 	static <T> MessageResult<T> acknowledge(MessageId messageId, T value) {
 		return ApiImplementationFactory.acknowledge(messageId, value);
 	}
 
+	/**
+	 * Returns the processed value and signals that the message must be negatively
+	 * acknowledged.
+	 * @param <T> the type of the processed value
+	 * @param messageId the id of the message to negatively acknowledge
+	 * @param value the processed value
+	 * @return the result of the message processing
+	 */
 	static <T> MessageResult<T> negativeAcknowledge(MessageId messageId, T value) {
 		return ApiImplementationFactory.negativeAcknowledge(messageId, value);
 	}
 
+	/**
+	 * Returns an empty value and signals that the message must be acknowledged.
+	 * @param messageId the id of the message to acknowledge
+	 * @return the result of the message processing
+	 */
 	static MessageResult<Void> acknowledge(MessageId messageId) {
 		return ApiImplementationFactory.acknowledge(messageId);
 	}
 
+	/**
+	 * Returns an empty value and signals that the message must be negatively
+	 * acknowledged.
+	 * @param messageId the id of the message to negatively acknowledge
+	 * @return the result of the message processing
+	 */
 	static MessageResult<Void> negativeAcknowledge(MessageId messageId) {
 		return ApiImplementationFactory.negativeAcknowledge(messageId);
 	}
 
+	/**
+	 * Returns an empty value and signals that the message must be acknowledged.
+	 * @param <V> the type of message payload
+	 * @param message the message to acknowledge
+	 * @return the result of the message processing
+	 */
 	static <V> MessageResult<Void> acknowledge(Message<V> message) {
 		return acknowledge(message.getMessageId());
 	}
 
+	/**
+	 * Returns an empty value and signals that the message must be negatively
+	 * acknowledged.
+	 * @param <V> the type of message payload
+	 * @param message the message to negatively acknowledge
+	 * @return the result of the message processing
+	 */
 	static <V> MessageResult<Void> negativeAcknowledge(Message<V> message) {
 		return negativeAcknowledge(message.getMessageId());
 	}
 
+	/**
+	 * Returns a message and signals that it must be acknowledged.
+	 * @param <V> the type of message payload
+	 * @param message the message
+	 * @return the result of the message processing
+	 */
 	static <V> MessageResult<Message<V>> acknowledgeAndReturn(Message<V> message) {
 		return acknowledge(message.getMessageId(), message);
 	}
 
+	/**
+	 * Gets whether the message must be acknowledged.
+	 * @return true if the message must be acknowledged
+	 */
 	boolean isAcknowledgeMessage();
 
+	/**
+	 * Gets the id of the message to acknowledge or negatively acknowledge.
+	 * @return the id of the message
+	 */
 	MessageId getMessageId();
 
+	/**
+	 * Gets the value processed by the message processing.
+	 * @return the value
+	 */
 	T getValue();
 
 }
