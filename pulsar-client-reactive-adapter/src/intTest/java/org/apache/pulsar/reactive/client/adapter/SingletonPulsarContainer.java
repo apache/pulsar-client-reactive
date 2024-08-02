9127e85cffed78eb7eadd9024780c7bffd7d4bfc
Lari Hotari <lhotari@users.noreply.github.com>
2024-08-02 19:01:45 +0300

    Upgrade Pulsar to 3.3.1 (#179)

--- pulsar-client-reactive-adapter/src/intTest/java/org/apache/pulsar/reactive/client/adapter/SingletonPulsarContainer.java
+++ pulsar-client-reactive-adapter/src/intTest/java/org/apache/pulsar/reactive/client/adapter/SingletonPulsarContainer.java
@@ -45,7 +45,7 @@ final class SingletonPulsarContainer {
 	}
 
 	static DockerImageName getPulsarImage() {
-		return DockerImageName.parse("apachepulsar/pulsar:3.2.3");
+		return DockerImageName.parse("apachepulsar/pulsar:3.3.1");
 	}
 
 }
